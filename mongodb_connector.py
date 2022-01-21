63dc361e8503d0d354419347e00401b7f889a0b4
Shiying Tu <shiyingtu@gmail.com>
2022-01-21 11:48:37 -0800

    Fix semgrep tests

--- mongodb_connector.py
+++ mongodb_connector.py
@@ -16,6 +16,7 @@
 #
 # phantom imports
 import json
+from socket import timeout
 
 import phantom.app as phantom
 import requests
@@ -303,6 +304,7 @@ class MongodbConnector(BaseConnector):
 
 def main():
     import argparse
+    import sys
 
     import pudb
 
@@ -313,12 +315,15 @@ def main():
     argparser.add_argument('input_test_json', help='Input Test JSON file')
     argparser.add_argument('-u', '--username', help='username', required=False)
     argparser.add_argument('-p', '--password', help='password', required=False)
+    argparser.add_argument('-v', '--verify', action='store_true', help='verify', required=False, default=False)
 
     args = argparser.parse_args()
     session_id = None
 
     username = args.username
     password = args.password
+    verify = args.verify
+    timeout = 30
 
     if username is not None and password is None:
 
@@ -331,7 +336,7 @@ def main():
             login_url = MongodbConnector._get_phantom_base_url() + '/login'
 
             print("Accessing the Login page")
-            r = requests.get(login_url, verify=False)
+            r = requests.get(login_url, verify=verify, timeout=timeout)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -344,11 +349,11 @@ def main():
             headers['Referer'] = login_url
 
             print("Logging into Platform to get the session id")
-            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
+            r2 = requests.post(login_url, verify=verify, data=data, headers=headers, timeout=timeout)
             session_id = r2.cookies['sessionid']
         except Exception as e:
             print("Unable to get session id from the platform. Error: " + str(e))
-            exit(1)
+            sys.exit(1)
 
     with open(args.input_test_json) as f:
         in_json = f.read()
@@ -365,7 +370,7 @@ def main():
         ret_val = connector._handle_action(json.dumps(in_json), None)
         print(json.dumps(json.loads(ret_val), indent=4))
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
